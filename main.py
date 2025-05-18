9250cdfbd19c16bf9b292bd5dc123e871bd1966e
kayra618 <kayracengizz58@gmail.com>
2025-05-18 09:51:16 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+import streamlit as st
+import xml.etree.ElementTree as et
+import sqlitecloud
+import sqlite3
+import requests
+from datetime import date
+from google import genai
+from pydantic import BaseModel
+
+conn=sqlitecloud.connect('sqlitecloud://cwcgjb0ahz.g1.sqlite.cloud:8860/chinook.sqlite?apikey=DaG8uyqMPa9GdxoR7ObMoajHIdfUOrc7B0mF0IrU6Y0')
+c=conn.cursor()
+
+c.execute("SELECT * FROM haberler ORDER BY LIMIT 99")
+haberler=c.fetchall()
+
+for i in range(haberler):
+    st.image(haberler[i][3])
+    st.write(haberler[i][1])
+    st.link_button("Habere git",haberler[i][2])
